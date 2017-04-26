c5a4b83828ca3185f71865e8bd02815b739a26d6
aidan <aidan@DESKTOP-QAP65M1>
2017-04-26 18:01:36 +0300

    testing inline

--- Amaranth/Object/Entity.cpp
+++ Amaranth/Object/Entity.cpp
@@ -4,46 +4,7 @@ Entity::Entity()
 {
 }
 
-void Entity::setAR(float ar) {
-	this->AR = ar;
-}
-
-void Entity::setTexture(Texture _texture)
+Entity::~Entity()
 {
-}
-
-void Entity::setCoordX(float x)
-{
-	this->coordX = x;
-}
-
-void Entity::setCoordY(float y)
-{
-	this->coordY = y;
-}
-
-void Entity::setHitPoints(float hp)
-{
-	this->hitPoints = hp;
-}
-
-float Entity::getCoordX()
-{
-	return coordX;
-}
-
-float Entity::getCoordY()
-{
-	return coordY;
-}
-
-float Entity::getHitPoints()
-{
-	return hitPoints;
-}
-
-Texture Entity::getTexture()
-{
-	return Texture();
 }
 
--- Amaranth/Object/Headers/Entity.h
+++ Amaranth/Object/Headers/Entity.h
@@ -7,17 +7,18 @@ class Entity {
 public:
 	//CONSTRUCTORS
 	Entity();
+	~Entity();
 	//SETTERS
-	void setAR(float ar);
-	void setTexture(Texture _texture);
-	void setCoordX(float x);
-	void setCoordY(float y);
-	void setHitPoints(float hp);
+	inline void setAR(float ar);
+	inline void setTexture(Texture _texture);
+	inline void setCoordX(float x);
+	inline void setCoordY(float y);
+	inline void setHitPoints(float hp);
 	//GETTERS
-	Texture getTexture();
-	float getCoordX();
-	float getCoordY();
-	float getHitPoints();
+	inline Texture getTexture();
+	inline float getCoordX();
+	inline float getCoordY();
+	inline float getHitPoints();
 protected:
 	float speed; //entity speed
 	float AR; 
@@ -26,5 +27,50 @@ protected:
 	float coordY = 0.0;
 	float hitPoints = 0.0;
 };
+
+inline void Entity::setAR(float ar) {
+	this->AR = ar;
+}
+
+inline void Entity::setTexture(Texture _texture)
+{
+}
+
+inline void Entity::setCoordX(float x)
+{
+	this->coordX = x;
+}
+
+inline void Entity::setCoordY(float y)
+{
+	this->coordY = y;
+}
+
+inline void Entity::setHitPoints(float hp)
+{
+	this->hitPoints = hp;
+}
+
+inline float Entity::getCoordX()
+{
+	return coordX;
+}
+
+inline float Entity::getCoordY()
+{
+	return coordY;
+}
+
+inline float Entity::getHitPoints()
+{
+	return hitPoints;
+}
+
+inline Texture Entity::getTexture()
+{
+	return Texture();
+}
+
+
 
 #endif  //_ENTITY_H
